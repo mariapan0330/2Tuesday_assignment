c6bd3c54206dfacc66b536c155f4c5c5f5a41b29
mariapan0330 <maria.pan0330@gmail.com>
2022-08-20 12:33:57 -0500

    changed password length to 99

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(99), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     contacts = db.relationship('Contact', backref='author', lazy='dynamic')
